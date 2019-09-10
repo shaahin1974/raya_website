088d2d48a574a93bd85328c969b4c081839cb5c4
Shaahin Chiachi <54498193+shaahin1974@users.noreply.github.com>
2019-09-11 00:10:30 +0430

    Update settings.py

--- raya_website/settings.py
+++ raya_website/settings.py
@@ -120,3 +120,8 @@ STATICFILES_DIRS = [
 # Media Folder Settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
