2ace715795fac534cb31ea8145880f7483193fd2
Shaahin Chaichi <shaahin.chaichi@gmail.com>
2019-09-10 23:37:45 +0430

    Database settings

--- raya_website/settings.py
+++ raya_website/settings.py
@@ -5,10 +5,6 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '_&ve$y5^upyqi+wj#-zd_!ou(&-izg*adqlagigj673!rwn9mc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -66,10 +62,14 @@ WSGI_APPLICATION = 'raya_website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rayadb',
+        'USER': 'postgres',
+        'PASSWORD': 'p@ssw0rd342',
+        'HOST': 'localhost'
     }
 }
+
 
 
 # Password validation
