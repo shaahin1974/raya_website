e7bf4cd2c53faf6e3aebbadcff0a85586323348e
Shaahin Chiachi <54498193+shaahin1974@users.noreply.github.com>
2019-09-11 11:53:13 +0430

    Update local_settings.py
    STATIC and MEDIA configuration added

--- raya_website/local_settings.py
+++ raya_website/local_settings.py
@@ -15,3 +15,15 @@ DATABASES = {
         'HOST': 'localhost'
     }
 }
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'raya_website/static')
+]
+
+# Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
