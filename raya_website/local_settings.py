09c3812f7f84554b5fff8f830ea83bebdcb67289
Shaahin Chiachi <54498193+shaahin1974@users.noreply.github.com>
2019-09-11 11:57:04 +0430

    Update local_settings.py

--- raya_website/local_settings.py
+++ raya_website/local_settings.py
@@ -1,3 +1,8 @@
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 SECRET_KEY = '_&ve$y5^upyqi+wj#-zd_!ou(&-izg*adqlagigj673!rwn9mc'
 
