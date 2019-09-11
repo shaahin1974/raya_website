d0de7c5c95ac5920d69919aa150d7c2d33aa04e0
Shaahin Chiachi <54498193+shaahin1974@users.noreply.github.com>
2019-09-11 11:59:51 +0430

    Update local_settings.py

--- raya_website/local_settings.py
+++ raya_website/local_settings.py
@@ -1,7 +1,3 @@
-import os
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 SECRET_KEY = '_&ve$y5^upyqi+wj#-zd_!ou(&-izg*adqlagigj673!rwn9mc'
@@ -21,14 +17,3 @@ DATABASES = {
     }
 }
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'raya_website/static')
-]
-
-# Media Folder Settings
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
