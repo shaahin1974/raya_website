15ea7a07f1090ace206b00fc16e9485e9184e96a
Shaahin Chiachi <54498193+shaahin1974@users.noreply.github.com>
2019-09-11 11:44:13 +0430

    Update local_settings.py
    Host IP Added

--- raya_website/local_settings.py
+++ raya_website/local_settings.py
@@ -2,14 +2,14 @@
 SECRET_KEY = '_&ve$y5^upyqi+wj#-zd_!ou(&-izg*adqlagigj673!rwn9mc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['5.253.24.56']
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'rayadb',
+        'NAME': 'raya_prod',
         'USER': 'dbadmin',
         'PASSWORD': 'p@ssw0rd342',
         'HOST': 'localhost'
