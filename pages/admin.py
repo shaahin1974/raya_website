8a94f959677420c1dc2878e2ed43ce54ab83ef95
Shaahin Chaichi <shaahin.chaichi@gmail.com>
2019-09-14 00:24:30 +0430

    Admin Area Configurations

--- pages/admin.py
+++ pages/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 
 from .models import ServiceArea
 
-admin.site.register(ServiceArea)
+
+class PagesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'author')
+    list_display_links = ('id', 'title')
+    list_filter = ('author',)
+    search_fields = ('title', 'description')
+    list_per_page = 25
+
+
+admin.site.register(ServiceArea, PagesAdmin)
--- pages/models.py
+++ pages/models.py
@@ -4,11 +4,12 @@ from django.db import models
 
 
 class ServiceArea(models.Model):
-    title = models.CharField(max_length=20)
-    content = models.TextField(max_length=200)
-    description = models.CharField(max_length=50)
+    title = models.CharField(max_length=60)
+    content = models.TextField(max_length=800)
+    description = models.CharField(max_length=60)
     author = models.CharField(max_length=20)
     photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    is_published = models.BooleanField(default=True)
     list_date = models.DateTimeField(default=datetime.now, blank=True)
 
     def __str__(self):
