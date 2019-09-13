15a2570fa5c45546ab7b1071144953dac8ce28f3
Shaahin Chaichi <shaahin.chaichi@gmail.com>
2019-09-13 20:05:56 +0430

    pages table fields

--- pages/models.py
+++ pages/models.py
@@ -10,5 +10,6 @@ class ServiceArea(models.Model):
     author = models.CharField(max_length=20)
     photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
     list_date = models.DateTimeField(default=datetime.now, blank=True)
+
     def __str__(self):
         return self.title
