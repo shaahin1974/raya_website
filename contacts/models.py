952e82bf5171c1d7499b4840974e1b3f94bf51c1
Shaahin Chaichi <shaahin.chaichi@gmail.com>
2019-09-19 23:49:05 +0430

    enquiry form date Filed added

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db import models
 
 
@@ -7,5 +9,7 @@ class Contact(models.Model):
     email = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
     message = models.TextField(blank=True)
+    contact_date = models.DateTimeField(default=datetime.now, blank=True)
+
     def __str__(self):
         return self.name
